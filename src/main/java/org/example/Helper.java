674e91c46a42a752be08bfc07f0d72aa82d68ab4
shresthsahai <shresth.sahai@aktana.com>
2025-01-18 12:15:27 +0530

    code 18 jan

---
+++ src/main/java/org/example/Helper.java
@@ -0,0 +1,172 @@
+package org.example;
+
+public class Helper {
+    // variables -> container -> holds a value
+    // data type variable_name = value
+    // java is static typed ->
+    // java is strongly types restriction on what vals can be assigned
+
+    // rules of naming ->
+    //1 varibale name is case sensitive
+    //2 variable name can start with letter , dollar ,_
+    //3 reserved keywords not allowed -> main ,static , void etc
+    int x=1;
+
+}
+/*
+Which of the following variable names is valid in Java?
+A) 1stNumber
+B) _firstNumber -> correct
+C) first-number (-  -> subtraction)
+D) first number -> space can't be in between
+
+Which of the following is NOT allowed as a variable name in Java?
+A) myVar
+B) MyVar
+C) $myVar
+D) class -> reserved word
+
+Which of the following variable names follows the standard Java naming
+conventions for variables?
+A) EmployeeName
+B) employeeName correct answer -> camel
+C) employee_name
+D) EMPLOYEENAME
+
+camelcase -> use starts with lower letter guviClassesIndia
+
+int _count = 10;
+int total$ = 50;
+int 2ndValue = 30; -> X ->  cannot start with digit
+int number = 25;
+
+Which of the following statements is true regarding Java variable naming?
+
+A) Java variable names are case-insensitive.
+B) Java variable names can only contain letters and digits.
+C) Java variable names cannot use special characters except $ and _.
+D) Java variable names must start with a capital letter.
+
+
+
+Which of the following identifiers is a valid variable name in Java?
+
+A) 2cool4school
+B) Cool_4_School
+C) cool-4-school
+D) cool 4 school
+
+Which variable name violates Java’s naming rules?
+
+A) employee_salary
+B) _employeeSalary
+C) $employeeSalary
+D) employee@salary  X
+
+Which of the following names is acceptable for a constant (assuming it follows naming conventions)?
+
+A) MAX_LIMIT - crrct
+B) maxLimit
+C) MaxLimit
+D) Max_Limit
+
+Identify the variable name that is invalid:
+
+A) student2
+B) Student2
+C) 2student->  X
+D) student_2
+
+
+Which of the following is valid if used in a class (but not necessarily recommended for readability)?
+
+A) class
+B) switch
+C) intValue$
+D) new
+
+Which of the following declarations is legal in Java?
+
+A) double _3dCoordinate = 5.7; - crcct
+B) double 3dCoordinate = 5.7;
+C) double d-Coordinate = 5.7;
+D) double d Coordinate = 5.7;
+
+int $totalAmount = 250; ->
+
+Choose the valid identifier:
+
+A) userName! -> special chars are not allowed
+B) user_Name
+C) user Name
+D) user#Name
+
+Which of the following variable names adhere to standard Java naming conventions (camelCase) for a non-constant variable?
+
+A) employeeSalary
+B) EmployeeSalary
+C) employee_salary
+D) EMPLOYEESALARY
+
+
+Which variable name is valid?
+
+A) EmployeeName2nd -> crrct
+B) EmployeeName-2nd
+C) Employee Name2nd
+D) EmployeeName 2nd
+
+int value$1 = 100;
+int $value1 = 200;
+int value_1 = 300;
+int %value1 = 400;
+
+Which of the following variable names is recommended according to Java naming conventions for non-final fields?
+
+A) _price
+B) price
+C) Price
+D) p_r_i_c_e
+
+
+Which of the following declarations is valid?
+
+A) String my-String = "Hello";
+B) String myString = "Hello";
+C) String my String = "Hello";
+D) String myString! = "Hello";
+
+
+Which of the following is the correct identifier for use as a variable name in Java?
+
+A) total#sum
+B) totalSum
+C) total#Sum
+D) total Sum
+
+
+Given the identifier var_1$, which statement is true?
+
+A) It is invalid because it ends with a dollar sign.
+B) It is invalid because identifiers cannot contain underscores.
+C) It is valid as it only contains letters, digits, underscores, and dollar signs.
+D) It is invalid because it starts with 'var' which is reserved.
+
+
+Which of these names would be poor practice even though it is valid?
+
+A) i
+B) tempValue
+C) a
+D) numberOfStudents
+
+
+Identify the option(s) that represent valid variable names in Java (choose all that apply):
+
+A) firstName
+B) LastName
+C) first_name
+D) firstName!
+
+
+ */
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,72 @@
 package org.example;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+// **** only 1 public class is in java
+public class Main { // class dec,
+    // public -> access specifier
+
+    // static -> method belong to class itself
+    // void  retunr type
+    // main -> name of the method
+    // string[] args -> array of params
+    // JVM -> main method in main class
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+// user input
+//        Scanner scanner=new Scanner(System.in);
+//        System.out.print("Enter a name ");
+//        String name= scanner.nextLine();// reads a line of input
+//
+//        System.out.print(" name " + name + "!");
+//
+//            scanner.close();
+
+//        System.out.print("Hello world!");
+
+ // buffered reader -> try catch
+
+        BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter a name ");
+        try{
+            String name=reader.readLine();
+            System.out.print(" name " + name + "!");
+        }
+        catch (IOException e){
+            System.out.print("Err detected!");
+        }
+
+
+
+
+
+        // prining statements
+//        System.out.println("Hello world!");
+//        System.out.print("Hello world!");
+//        System.out.print("Hello world!");
+
     }
 }
+
+// java ->  high level oop (classes & objects) -> Sun Microsystems (oracle) 1990s
+// Write once run anywhere (WORA)
+// Platform independence -> Java Program - >
+//Java Program - >  compiled into byte code -> intermediate lang executed by JVM
+
+// 3 components of java -> JVM -> java virtual machine ->  byte code -> machine code
+// java program -> compiler -> byte code -> JVM -> Machine Code -> CPU -> Output
+// java is platform independent but JVM is platform dependent
+// JRE -> java runtime ENV   ->  JVM + lib
+// JDK -> JRE + programing lang info + compiler (javac) + debugger
+// JDK = JRE +  programing lang info + compiler (javac) + debugger
+
+// .java (source code ) -> javac (byte code + stored in class file ) -> JVM
+// versions -> JSE -> java stnd edition -> core java
+// JEE -> java enterprise edition -> full stack dev
+// JME -> java micro/mobile  edition -> mobile dev
+
+
+
+
